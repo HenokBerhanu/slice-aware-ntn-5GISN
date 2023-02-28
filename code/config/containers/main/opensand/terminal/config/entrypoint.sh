1666dc929dbedb230efad9be219fa323430299f9
zbh888 <b327zhan@uwaterloo.ca>
2023-02-28 17:22:04 -0500

    update entrypoint for opensand

--- code/config/containers/main/opensand/terminal/config/entrypoint.sh
+++ code/config/containers/main/opensand/terminal/config/entrypoint.sh
@@ -2,4 +2,7 @@
 service dbus start
 service avahi-daemon start
 opensand_interfaces
+ip route add 172.16.8.0/24 via 172.16.5.2
+ip route add 172.16.0.0/24 via 172.16.100.2
+ip route add 172.16.1.0/24 via 172.16.100.2
 sand-daemon -f
